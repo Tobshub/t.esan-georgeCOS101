4e12f6f739632a01c5c9186222dd69b688acb2d7
Tobani <tobanigeorge@gmail.com>
2024-10-21 10:34:21 +0100

    week 2 practice 1

---
+++ week-2/practice_1.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Welcome to COS 101!");
+
+    println!("The course learning outcome is as follows!");
+
+    println!(
+        "\
+I can't see the text right now,, but seems we just want \n
+multiline text.\n
+Dr Desmond accidentally added in /n instead onf \n"
+    )
+}
