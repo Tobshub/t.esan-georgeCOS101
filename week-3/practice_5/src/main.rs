71f3b7001d2450a494e6fe9a13ae44ddde4de124
Tobani <tobanigeorge@gmail.com>
2024-10-28 11:48:34 +0100

    week 3 practices

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@';
+    let alphabet: char = 'B';
+    let surname = "Bolaji";
+    let first_name = "Michael";
+    let middle_name = "Gboyega";
+
+    println!(" ");
+    println!("Special character: {}", special_character);
+    println!("Alphabet: {}", alphabet);
+    println!("Name: {} {} {}", first_name, middle_name, surname);
+}
